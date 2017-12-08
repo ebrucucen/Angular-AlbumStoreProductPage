2a93bbb10a5250ba23062ece814ca697d7b37553
EbruCucen <ebrugul@hotmail.com>
2017-12-08 11:01:36 +0000

    part5:@

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -8,14 +8,15 @@ import { ProductService } from '../product.service';
   styleUrls: ['./product-tracklisting.component.css']
 })
 export class ProductTracklistingComponent implements OnInit {
-  albumInfo:Album;
+  albumInfo: Album;
 
-  constructor(private _productService:ProductService) { 
+  constructor(private _productService: ProductService) { 
 
   }
 
   ngOnInit() {
-    this._productService.getAlbum(1).subscribe(response=>this.albumInfo=response);
+    this._productService.getAlbum(1).subscribe(response=>
+      this.albumInfo = response);
   } 
 
 }
